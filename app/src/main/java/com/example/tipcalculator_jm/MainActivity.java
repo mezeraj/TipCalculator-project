bcbca1f8b12b37e06fdb76b5b7de293e0166640c
mezeraj <mezeraj@g.cofc.edu>
2020-03-05 17:52:48 -0500

    This is it I guess

--- app/src/main/java/com/example/tipcalculator_jm/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_jm/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.tipcalculator_jm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Button calcBtn;
+    private Button clearBtn;
+    private RadioButton twentyFive;
+    private RadioButton twenty;
+    private RadioButton fifteen;
+    private RadioButton optionBtn;
+    private TextInputLayout tipEntry;
+    private TextInputLayout personEntry;
+    private TextInputLayout customtip;
+    private TextView tipTotal;
+    private TextView tipPerPerson;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calcBtn = findViewById(R.id.)
+
     }
 }
